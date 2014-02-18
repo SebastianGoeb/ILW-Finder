08bab90f85bbef84993522e53293e16c069f5fc2
magniffico <martynas.melninkas@gmail.com>
2014-02-18 14:53:23 +0000

    template added

--- src/dbmodel.py
+++ src/dbmodel.py
@@ -2,7 +2,6 @@ from google.appengine.ext import ndb
 
 # ndb database classes
 class Postcodes (ndb.Model):
-    id = ndb.IntegerProperty()
     postcode = ndb.StringProperty()
     x_coord = ndb.FloatProperty()
     y_coord = ndb.FloatProperty()
@@ -10,52 +9,33 @@ class Postcodes (ndb.Model):
     @classmethod
     def get(cls):
         return cls.query().fetch()
-    @classmethod
-    def get_id(cls, id):
-        return cls.query().fetch(1)
 
 class District (ndb.Model):
-    id = ndb.IntegerProperty()
     name = ndb.StringProperty(indexed = False)
     
     @classmethod
     def get(cls):
         return cls.query().fetch()
-    @classmethod
-    def get_id(cls, id):
-        return cls.query().fetch(1)
 
 class Person (ndb.Model):
-    id = ndb.IntegerProperty()
     district_id = ndb.IntegerProperty()
     postcode_id = ndb.IntegerProperty()
     
     @classmethod
     def get(cls):
         return cls.query().fetch()
-    @classmethod
-    def get_id(cls, id):
-        return cls.query().fetch(1)
 
 class PlaceCat(ndb.Model):
-    id = ndb.IntegerProperty()
     cat_name = ndb.StringProperty()
     
     @classmethod
     def get(cls):
         return cls.query().fetch()
-    @classmethod
-    def get_id(cls, id):
-        return cls.query().fetch(1)
     
 class PublicPlace(ndb.Model):
-    id = ndb.IntegerProperty()
     cat_id = ndb.IntegerProperty()
     postcode_id = ndb.IntegerProperty()
     
     @classmethod
     def get(cls):
         return cls.query().fetch()
-    @classmethod
-    def get_id(cls, id):
-        return cls.query().fetch(1)
--- src/index.py
+++ src/index.py
@@ -1,48 +1,18 @@
-from __future__ import print_function
-from flask import Flask
+from flask import Flask, render_template
 from dbmodel import *
 from google.appengine.ext import ndb
 import simplejson as json
 from StringIO import StringIO
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Index cia Flask'
-
+	return render_template("main.html")
 
 @app.route('/get/postcodes')
 def getPostcodes():
     output = StringIO()
-    
     data = [x.to_dict() for x in Postcodes.get()]
     json.dump(data,output)
     return output.getvalue()
-    
-
-# ndb database classes
-#class PostCode (ndb.Model):
-#    id = int()
-#    postocde = ndb.StringProperty(indexed=False)
-#    x_coord = float()
-#    y_coords = float()
-#
-#class District (ndb.Model):
-#    id = int()
-#    name = ndb.StringProperty(indexed = False)
-#
-#class Person (ndb.Model):
-#    id = int()
-#    district_id = int()
-#    postcode_id = int()
-#
-#class PlaceCat(ndb.Model):
-#    id = int()
-#    cat_name = ndb.StringProperty(indexed = False)
-#    
-#class PublicPlace(ndb.Model):
-#    id = int()
-#    cat_id = int()
-#    postcode_id = int()
