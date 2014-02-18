f97e4cd3bf306ec17fc42f39d4fb654249ffc392
Roberth Kulbin <roberth@winter-weht.net>
2014-02-18 11:49:43 +0000

    separate DB model

---
+++ src/dbmodel.py
@@ -0,0 +1,26 @@
+from google.appengine.ext import ndb
+
+# ndb database classes
+class PostCode (ndb.Model):
+    id = int()
+    postocde = ndb.StringProperty(indexed=False)
+    x_coord = float()
+    y_coords = float()
+
+class District (ndb.Model):
+    id = int()
+    name = ndb.StringProperty(indexed = False)
+
+class Person (ndb.Model):
+    id = int()
+    district_id = int()
+    postcode_id = int()
+
+class PlaceCat(ndb.Model):
+    id = int()
+    cat_name = ndb.StringProperty(indexed = False)
+    
+class PublicPlace(ndb.Model):
+    id = int()
+    cat_id = int()
+    postcode_id = int()
