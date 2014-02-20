bc95cf1a67f50facda4ceab01f77d9ca4371cb1b
milliburn <roberth@winter-weht.net>
2014-02-20 20:25:15 +0000

    current state

--- app/src/static/map.js
+++ app/src/static/map.js
@@ -9,14 +9,70 @@ var districts = {};
 
 var map;
 
-function Node() {
+function Node(d) {
     this.postcode = undefined;
     this.georef = undefined;
-    this.district = undefined;
+    this.district = d;
+    this.district_inner_range = [];
 
-    function getTwoClosestInDistrict() {
-        out = [undefined, undefined];
-        outRng = [undefined, undefined];
+    this.infoWindow = new google.maps.InfoWindow({
+        content: d.name,
+    });
+    
+    this.calc_district_inner_range = function() {
+        if (this.district_inner_range.length != 0) {
+            return this.district_inner_range;
+        }
+
+        var out = [];
+
+        for (var n in this.district.nodes) {
+            node = this.district.nodes[n]
+            
+            if (node == this) {
+                continue;
+            }
+
+            out.push(
+                [node,
+                 google.maps.geometry.spherical.computeDistanceBetween(
+                     node.georef, this.georef)]);
+        }
+
+        // sort in ascending range order
+        this.district_inner_range = out.sort(
+            function(a,b) {
+                return a[1] - b[1];
+            }
+        );
+
+        return this.district_inner_range;
+    }
+
+    this.closest_in_district = function(num) {
+        lst = this.calc_district_inner_range();
+
+        num = Math.min(num, lst.length);
+
+        return lst.slice(0,num-1)
+    };
+
+
+    this.drawCircle = function() {
+        var circ = new google.maps.Circle({
+            strokeOpacity: 0.0,
+            fillColor: this.district.colour,
+            fillOpacity: 0.5,
+            map: map,
+            center: this.georef,
+            radius: 100
+        });
+
+        google.maps.event.addListener(circ, 'click', function() {
+            console.log(this);
+            console.log(this.infoWindow);
+            this.infoWindow.open(map, this.georef);
+        });
     };
 }
 
@@ -32,6 +88,28 @@ function District() {
     };
 
     this.colour = _nextColour();
+
+    this.drawInnerPolygons = function() {
+        if (this.nodes.length < 3) {
+            return;
+        }
+
+        for (var n in this.nodes) {
+            vertices = this.nodes[n].closest_in_district(3).map(function(n_) {
+                return n_[0].georef;
+            });
+
+            vertices.push(this.nodes[n].georef)
+
+            new google.maps.Polygon({
+                paths: vertices,
+                strokeOpacity: 0,
+                fillColor: this.colour,
+                fillOpacity: 0.5,
+                map: map
+            });
+        }
+    };
 }
 
 $.get( "get/district/of-postcodes", function (data) {
@@ -53,8 +131,7 @@ $.get( "get/district/of-postcodes", function (data) {
             d = districts[district_name];
         }
 
-        n = new Node();
-        n.district = d;
+        n = new Node(d);
         n.postcode = pc;
         d.nodes.push(n);
 
@@ -86,15 +163,12 @@ function initmap() {
     
     map = new google.maps.Map(document.getElementById("map-canvas"),
 		                          mapOptions);
-    for (var pc in nodes) {
-        nodes[pc].circ =
-            new google.maps.Circle({
-                strokeOpacity: 0.0,
-                fillColor: nodes[pc].district.colour,
-                fillOpacity: 0.5,
-                map: map,
-                center: nodes[pc].georef,
-                radius: 40
-            });
+
+    // for (var d in districts) {
+    //     districts[d].drawInnerPolygons();
+    // }
+
+    for (var n in nodes) {
+        nodes[n].drawCircle();
     }
 }
